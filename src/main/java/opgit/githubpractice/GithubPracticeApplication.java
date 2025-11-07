e0c900f48e497a9696cb69b20de9a028554ccbcd
orlovsoftdev <orlovsoftwaredeveloper@proton.me>
2025-11-07 12:06:29 +0200

    Init repository

---
+++ src/main/java/opgit/githubpractice/GithubPracticeApplication.java
@@ -0,0 +1,13 @@
+package opgit.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/opgit/githubpractice/GithubPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package opgit.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
